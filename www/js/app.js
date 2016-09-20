634cf51db8097a8fd803488bb3990fc3b577e6aa
Adrian Png <adrian.png@fuzziebrain.com>
2016-09-19 23:28:16 -0700

    Initial solution

---
+++ www/js/app.js
@@ -0,0 +1,69 @@
+(function() {
+  'use strict';
+
+  angular.module('ShoppingListCheckOff', ['ngMaterial'])
+    .config(function($mdThemingProvider, $mdIconProvider) {
+      $mdThemingProvider.theme('mytheme').primaryPalette('blue').accentPalette('blue-grey');
+    })
+    .service('shopper', ShoppingListCheckOffService)
+    .controller('ToBuyShoppingController', ToBuyShoppingController)
+    .controller('AlreadyBoughtShoppingController', AlreadyBoughtShoppingController);
+
+  ToBuyShoppingController.$inject = ['$scope', 'shopper'];
+  AlreadyBoughtShoppingController.$inject = ['$scope', 'shopper'];
+
+  function ShoppingListCheckOffService() {
+    var service = this;
+
+    var shoppingList = [
+      {
+        name: 'Cookie'
+        , quantity: 10
+      }
+      , {
+        name: 'Peanut'
+        , quantity: 20
+      }
+      , {
+        name: 'Apple'
+        , quantity: 5
+      }
+      , {
+        name: 'Mango'
+        , quantity: 2
+      }
+      , {
+        name: 'Potato Chip'
+        , quantity: 20
+      }
+    ];
+
+    var inventoryList = [];
+
+    service.getShoppingList = function() {
+      return shoppingList;
+    };
+
+    service.getInventoryList = function() {
+      return inventoryList;
+    };
+
+    service.addToInventory = function(index) {
+      var item = shoppingList[index];
+      shoppingList.splice(index, 1);
+      inventoryList.push(item);
+    }
+  }
+
+  function ToBuyShoppingController($scope, shopper) {
+    $scope.shoppingList = shopper.getShoppingList();
+
+    $scope.addToInventory = function(index) {
+      shopper.addToInventory(index);
+    }
+  }
+
+  function AlreadyBoughtShoppingController($scope, shopper) {
+    $scope.inventoryList = shopper.getInventoryList();
+  }
+})();
